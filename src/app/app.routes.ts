d1cb8e2ac4872402b4ee682c9c809bbc93d8b967
Gonzalo Viaytes <gonzaviaytes@gmail.com>
2024-01-12 11:08:30 -0300

    LandingA17

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'products/:id', component: ProductDetailComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+
+
+
+];
